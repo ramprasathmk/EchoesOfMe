da574166a93c1dff690a8210d6c18cd850ee654e
ramprasathmk <mkramprasath12@gmail.com>
2024-10-15 11:34:53 +0545

    Update files

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const StyledHome = styled.div`
   h1 {
@@ -7,12 +7,15 @@ const StyledHome = styled.div`
     text-align: center;
     font-size: 2.5rem;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
-    font-weight: calc(500/2);
+    font-weight: calc(500 / 2);
     -webkit-text-fill-color: #30b4b9c6;
   }
   h1:hover {
     -webkit-text-fill-color: #45f88d;
-
+  }
+  body {
+    margin: 0px;
+    padding: 0px;
   }
   p {
     font-size: 1.2rem;
@@ -24,15 +27,17 @@ const StyledHome = styled.div`
 function Home() {
   return (
     <StyledHome>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti earum
-        dolorum laboriosam numquam doloremque tenetur iure, dolorem est culpa
-        officiis repudiandae accusamus nostrum omnis veniam magni eos
-        consequuntur porro libero?
-      </p>
+      <div className="container-fluid">
+        <h1>Home</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+          earum dolorum laboriosam numquam doloremque tenetur iure, dolorem est
+          culpa officiis repudiandae accusamus nostrum omnis veniam magni eos
+          consequuntur porro libero?
+        </p>
+      </div>
     </StyledHome>
   );
 }
 
-export default  Home
+export default Home;
