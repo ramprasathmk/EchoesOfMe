3296cad19635e37ba26ed23cd3711a55d5dfef20
ramprasathmk <mkramprasath12@gmail.com>
2024-11-07 19:05:26 +0545

    feat: Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 import Home from './components/pages/Home'
@@ -8,6 +8,9 @@ import { HelloWorld } from './components'
 
 
 function App() {
+
+  const path = window.location.pathname === '/hello' ? '/hello' : '/helloworld'
+
   return (
     <>
       {/* Header */}
@@ -15,10 +18,11 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/hello" element={<HelloWorld />} />
-          <Route path="/helloworld" element={<HelloWorld />} />
+          <Route path={path} element={<HelloWorld />} />
+          <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
       </div>
 
