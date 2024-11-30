fa720f1002dbae3ffba78e5794936028fb25324f
ramprasathmk <mkramprasath12@gmail.com>
2024-11-30 15:58:42 +0545

    refactor: change routes alignment
    Signed-off-by: ramprasathmk <mkramprasath12@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import NavBar from './components/NavBar'
 import Home from './components/pages/Home'
 import Projects from './components/pages/Projects'
 import About from './components/pages/About'
-import { HelloWorld } from './components'
 
 
 function App() {
-
-  const path = window.location.pathname === '/hello' ? '/hello' : '/helloworld'
 
   return (
     <>
@@ -18,10 +15,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path={path} element={<HelloWorld />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
       </div>
